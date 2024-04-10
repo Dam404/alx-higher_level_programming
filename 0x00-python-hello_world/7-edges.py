b2f5a36528b12d12b1250258e6616df23280a893
Odeyemi Damilola <odeyemidamilola078@gmail.com>
2024-04-10 22:35:19 +0100

    This code cut, copy and paste

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
-word_last_2 = word[7:9]
+word_last_2 = word[-2:]
 middle_word = word[1:8]
 print(f"First 3 letter: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
---
+++ 0x00-python-hello_world/test.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = "Holberton"
+print(len(str))
