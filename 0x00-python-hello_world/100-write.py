442676a446107597d5d35f41bd4126b141648c04
Odeyemi Damilola <odeyemidamilola078@gmail.com>
2024-07-29 01:35:55 +0100

    This commit adds script to print a specific message to stderr and exit with the status code 1

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+#Writing the required message to stderr
+
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-1019\n")
+#sys.stderr.write is used to write this message to the standard error.
+
+#Exiting the script with the status code 1
+sys.exit(1)
+
